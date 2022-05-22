ebbe7b984404364b26f5afe3cdb9e5f11578c441
venuave <krisy2017@gmail.com>
2022-05-22 12:19:21 +0200

    Overview and Setup

--- app.py
+++ app.py
@@ -1,34 +1,15 @@
-print("Welcome! This is your shopping list!")
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+app.config.from_object('config')
 
-items=[]
+@app.route('/')
+def index():
+  return 'Hello World!'
 
-def add_item():
-  print("What item would you like to add?")
+@app.route('/about')
+def about():
+  return 'I like cookies'
 
-  item = input("> ")
-  items.append(item)
-  
-  print(items)
-
-  print("Do you want to add anything else? y/n?")
-  response = input("> ")
-
-  if response == "y":
-    add_item()
-  else:
-    print("Ok bye")
-    save_list()
-
-
-def save_list():
-  file = open("list.txt", "w")
-
-  for item in items:
-    file.write(item + "\n")
-  file.close()
-
-print ("Your list was saved!")
-
-add_item()
-  
+if __name__ == '__main__':
+  app.run()
 
