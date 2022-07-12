c2d2590f4526c8c9715096fed8e5bc88ee787b72
venuave <krisy2017@gmail.com>
2022-07-12 15:32:06 +0200

    validation and exception handling

--- app/orders/routes.py
+++ app/orders/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, current_app
 from app.cookies.models import Cookie
 from app.orders.models import Order, Address
 from .services.create_order import create_order
@@ -12,8 +12,27 @@ def get_checkout():
 
 @blueprint.post('/checkout')
 def post_checkout():
-  cookies = Cookie.query.all()
+  try:
+    cookies = Cookie.query.all()
 
-  create_order(request.form, cookies)
+    if not all([
+      request.form.get('name'),
+      request.form.get('street'),
+      request.form.get('city'),
+      request.form.get('state'),
+      request.form.get('zip'),
+      request.form.get('country')
+    ]):
+      raise Exception('Please fill out all address fields.')
 
-  return render_template('orders/new.html', cookies=cookies)
+    create_order(request.form, cookies)
+    return render_template('orders/new.html', cookies=cookies)
+  except Exception as error_message:
+    error = error_message or 'An error occurred while processing your order. Please make sure to enter valid data.'
+
+    current_app.logger.info(f'Error creating an order: {error}')
+
+    return render_template('orders/new.html', 
+      cookies=cookies,
+      error=error
+    )
