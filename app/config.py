bae574f39b9baa80901282b1843bead80e1eb303
venuave <krisy2017@gmail.com>
2022-07-12 17:02:39 +0200

    add Heroku configuration

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL').replace('postgres://', 'postgresql://', 1)
 
 COOKIES_PER_PAGE = 4
 
