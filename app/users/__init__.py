0917a6764db21fb3f33d8bd9ea1b7d66cc299a5c
venuave <krisy2017@gmail.com>
2022-07-26 12:07:40 +0200

    fixing usermixning and other small bugs that might cause trouble

--- app/users/__init__.py
+++ app/users/__init__.py
@@ -1,8 +1,2 @@
 from . import routes, models
 
-from app.extensions.database import db, CRUDMixin, UserMixin 
-
-class User(db.Model, CRUDMixin, UserMixin):
-  id = db.Column(db.Integer, primary_key = True)
-  email = db.Column(db.String(128), index = True, unique = True)
-  password = db.Column(db.String(1024))
