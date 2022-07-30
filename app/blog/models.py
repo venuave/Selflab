887cca86294511b9348afeb58f053d7391ab58ca
venuave <krisy2017@gmail.com>
2022-07-30 18:03:17 +0200

    changing model length to check heroku failing

--- app/blog/models.py
+++ app/blog/models.py
@@ -4,4 +4,4 @@ from app.extensions.database import db, CRUDMixin
 class BlogPost(db.Model, CRUDMixin, UserMixin):
   id = db.Column(db.Integer, primary_key = True)
   title = db.Column(db.String(128), index = True, unique = True)
-  body = db.Column(db.String(400))
+  body = db.Column(db.String(1024))
